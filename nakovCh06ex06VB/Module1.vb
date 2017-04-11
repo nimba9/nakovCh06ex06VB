9bd95c1d6c3d1f75a11a8202a0fe1a671eda36a4
vancisin peter <vancisin.peter@gmail.com>
2017-04-11 17:59:02 +0200

    Add project files.

---
+++ nakovCh06ex06VB/Module1.vb
@@ -0,0 +1,39 @@
+﻿Imports System
+Imports System.Collections.Generic
+Imports System.Linq
+Imports System.Text
+Imports System.Threading.Tasks
+
+Namespace nakovCh06ex06
+
+    Public Class Program
+
+        Public Shared Sub Main(ByVal args As String())
+            FibonacciSequence()
+        End Sub
+
+        Private Shared Sub FibonacciSequence()
+            Console.WriteLine("Please enter a positive number: ")
+            Dim userInp As String = Console.ReadLine()
+            Dim n As ULong = 0
+            Dim check As Boolean = ULong.TryParse(userInp, n)
+            If check <> True Then
+                Console.WriteLine("Please enter a positive number!")
+            End If
+
+            Dim firstNum As ULong = 1
+            Dim secondNum As ULong = 0
+            Dim thirdNum As ULong = 0
+            Dim sum As ULong = 0
+            For i As ULong = 1 To n
+                thirdNum = firstNum + secondNum
+                firstNum = secondNum
+                secondNum = thirdNum
+                sum += thirdNum
+                Console.WriteLine("{0}:{1}", i, thirdNum)
+            Next
+
+            Console.WriteLine("The sum is: {0}", sum)
+        End Sub
+    End Class
+End Namespace
